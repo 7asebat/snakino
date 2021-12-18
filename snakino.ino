8032f857c47db12ba5e40ae74084c9bb2b3e1479
Youssef Walid <yousefwalid98@hotmail.com>
2021-12-18 23:21:56 +0200

    DEV: Tweaks and bug fixes

--- snakino.ino
+++ snakino.ino
@@ -21,9 +21,20 @@ struct point
 #define PIN_CS 9
 #define PIN_CLK 10
 
+/************* analog stick pins *************/
+
+#define JOYSTICK_1_SW 6
+#define JOYSTICK_1_VRX A0
+#define JOYSTICK_1_VRY A1
+
+#define JOYSTICK_2_SW 7
+#define JOYSTICK_2_VRX A2
+#define JOYSTICK_2_VRY A3
+
 /************* game vars *************/
 float gameSpeed = 3;
 byte gameEnded = 0;
+long lastGameStep = 0;
 
 /************* snake vars *************/
 #define SNAKES_COUNT 1
@@ -36,14 +47,14 @@ ArduinoQueue<point> snkPoints[2] = {ArduinoQueue<point>(256), ArduinoQueue<point
 #define LED_MATRIX_INTENSITY 0 // 0..15, 0 is lowest
 #define LED_MATRIX_COUNT 2
 
-byte ledMatrixIdx[2][2] = {{0, 2}, {1, 3}};
+byte ledMatrixIdx[2][2] = {{0, 1}, {2, 3}};
 
 LedControl lc = LedControl(PIN_DIN, PIN_CLK, PIN_CS, LED_MATRIX_COUNT);
 
 /************* game matrix *************/
 
-#define GAME_MATRIX_W 8
-#define GAME_MATRIX_L 16
+#define GAME_MATRIX_W 16
+#define GAME_MATRIX_L 8
 
 #define GAME_MATRIX_CELL_EMPTY 0
 #define GAME_MATRIX_CELL_FOOD 100
@@ -57,6 +68,16 @@ point foodPos = {-1, -1};
 void initRandSeed()
 {
   randomSeed(analogRead(A5));
+}
+
+void initPinModes()
+{
+  pinMode(JOYSTICK_1_SW, INPUT);
+  pinMode(JOYSTICK_1_VRX, INPUT);
+  pinMode(JOYSTICK_1_VRY, INPUT);
+  pinMode(JOYSTICK_2_SW, INPUT);
+  pinMode(JOYSTICK_2_VRX, INPUT);
+  pinMode(JOYSTICK_2_VRY, INPUT);
 }
 
 void initLedMatrices()
@@ -74,7 +95,7 @@ void initSnakesOnGrid()
   gameMatrix[0][0] = snkId[0];
   gameMatrix[0][1] = snkId[0];
   gameMatrix[0][2] = snkId[0];
-  if(SNAKES_COUNT < 2)
+  if (SNAKES_COUNT < 2)
     return;
   gameMatrix[15][0] = snkId[1];
   gameMatrix[15][1] = snkId[1];
@@ -86,7 +107,7 @@ void initSnakesInQueue()
   snkPoints[0].enqueue(point{0, 0});
   snkPoints[0].enqueue(point{0, 1});
   snkPoints[0].enqueue(point{0, 2});
-  if(SNAKES_COUNT < 2)
+  if (SNAKES_COUNT < 2)
     return;
   snkPoints[1].enqueue(point{15, 0});
   snkPoints[1].enqueue(point{15, 1});
@@ -109,10 +130,15 @@ void printGridToMatrix()
       {
         ledMatrixIdxRow = i / 8;
         ledMatrixIdxCol = j / 8;
-        lc.setRow(ledMatrixIdx[ledMatrixIdxRow][ledMatrixIdxCol], i % 8, buffer);
+        lc.setColumn(ledMatrixIdx[ledMatrixIdxRow][ledMatrixIdxCol], 7 - (i % 8), buffer);
         buffer = 0x00;
       }
     }
+}
+
+/************* joystick functions *************/
+void takeJoystickInput()
+{
 }
 
 /************* util fns *************/
@@ -127,6 +153,16 @@ point evalNewPoint(point curPos, byte dir)
   else if (dir == DIR_LEFT)
     return point{curPos.i, curPos.j - 1};
   return curPos;
+}
+
+bool unblockingDelay(long *lastMillis, long delay)
+{
+  if (millis() - *lastMillis < delay)
+    return false;
+
+  *lastMillis = millis();
+
+  return true;
 }
 
 /************* game fns *************/
@@ -148,7 +184,7 @@ void stepSnakes()
     gameMatrix[newSnakeHead.i][newSnakeHead.j] = snakeId;
     snkPoints[snakeIdx].enqueue(newSnakeHead);
 
-    if(!snakeHeadOnFood(newSnakeHead))
+    if (!snakeHeadOnFood(newSnakeHead))
     {
       gameMatrix[snakeTail.i][snakeTail.j] = GAME_MATRIX_CELL_EMPTY;
       snkPoints[snakeIdx].dequeue();
@@ -169,7 +205,7 @@ void updateFoodPos()
 {
   if (gameMatrix[foodPos.i][foodPos.j] != GAME_MATRIX_CELL_FOOD)
     foodPos = point{-1, -1};
-  
+
   if (foodPos.i != -1 || foodPos.j != -1)
     return;
 
@@ -182,15 +218,13 @@ void updateFoodPos()
   foodPos.i = i, foodPos.j = j;
 }
 
-void checkGameEnd() {
-  for(int snakeIdx = 0; snakeIdx < SNAKES_COUNT; snakeIdx++)
+void checkGameEnd()
+{
+  for (int snakeIdx = 0; snakeIdx < SNAKES_COUNT; snakeIdx++)
   {
     point snakeHead = snkPoints[snakeIdx].getTail();
-    if(snakeHead.i >= GAME_MATRIX_L || snakeHead.i < 0  || snakeHead.j < 0 || snakeHead.j >= GAME_MATRIX_W)
+    if (snakeHead.i >= GAME_MATRIX_L || snakeHead.i < 0 || snakeHead.j < 0 || snakeHead.j >= GAME_MATRIX_W)
       gameEnded = 1;
-
-//    if(gameMatrix[snakeHead.i][snakeHead.j] != GAME_MATRIX_CELL_EMPTY || gameMatrix[snakeHead.i][snakeHead.j] != GAME_MATRIX_CELL_FOOD)
-//      gameEnded = 1;
   }
 }
 
@@ -199,19 +233,18 @@ void stepGame()
   stepSnakes();
   checkGameEnd();
   updateFoodPos();
-  //  printGameMatrix();
 }
 
 void endGame()
 {
-  for(int i = 0; i < GAME_MATRIX_L; i++)
-    for(int j = 0; j < GAME_MATRIX_W; j++)
+  for (int i = 0; i < GAME_MATRIX_L; i++)
+    for (int j = 0; j < GAME_MATRIX_W; j++)
       gameMatrix[i][j] = GAME_MATRIX_CELL_EMPTY;
   printGridToMatrix();
 }
 
 /************* dummy fns *************/
-void takeInput()
+void takeKeyboardInput()
 {
   while (Serial.available())
   {
@@ -240,26 +273,32 @@ void printGameMatrix()
   }
 }
 
+/************* setup *************/
 void setup()
 {
   Serial.begin(9600);
   initRandSeed();
+  initPinModes();
   initLedMatrices();
   initSnakesInQueue();
   initSnakesOnGrid();
   printGridToMatrix();
 }
 
+/************* loop *************/
 void loop()
 {
-  delay(int(1000.0 / gameSpeed));
-  takeInput();
-  stepGame();
-  if(gameEnded)
+  takeKeyboardInput();
+
+  if (unblockingDelay(&lastGameStep, long(1000.0 / gameSpeed)))
+    stepGame();
+
+  if (gameEnded)
   {
     endGame();
-    while(1);
+    while (1)
+      ;
   }
-    
+
   printGridToMatrix();
 }
